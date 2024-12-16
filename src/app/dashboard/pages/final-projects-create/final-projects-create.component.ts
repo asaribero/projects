8bf79bdb460258a2cf099a556de0db49ed0a5aad
leaderdevelopment1 <164250794+leaderdevelopment1@users.noreply.github.com>
2024-12-15 19:12:32 -0500

    Update final-projects-create.component.ts

--- src/app/dashboard/pages/final-projects-create/final-projects-create.component.ts
+++ src/app/dashboard/pages/final-projects-create/final-projects-create.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CategoriasService } from '../../../services/categorias/categorias.service';
 import internal from 'stream';
 import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ProjectsService } from '../../../services/projects/projects.service';
 import { ModalComponent } from '../../../shared/modal/modal/modal.component';
 import { SwitchService } from '../../../services/modal/switch.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-final-projects-create',
@@ -23,6 +24,7 @@ export default class FinalProjectsCreateComponent {
   routerLinkBtn1: string = '';
   textBtn1: string = '';
   textBtn2: string = '';
+  private router = inject(Router);
   constructor(
     private categoriasService: CategoriasService,
     private projectsService: ProjectsService,
@@ -42,12 +44,35 @@ export default class FinalProjectsCreateComponent {
   }
 
   nuevoProyecto(): void {
-    if(this.saveObj.titulo != '' && this.saveObj.resumen != '' && this.saveObj.idCategoria != 0){
-      this.projectsService.guardarProyecto(this.saveObj).subscribe((res: any) => {
-          this.openDialog(res.status);
+    if (this.saveObj.titulo != '' && this.saveObj.resumen != '' && this.saveObj.idCategoria != 0) {
+      this.projectsService.guardarProyecto(this.saveObj).subscribe(
+        (res: any) => {
+          Swal.fire({
+            title: 'Éxito',
+            text: 'Proyecto guardado correctamente.',
+            icon: 'success',
+            confirmButtonText: 'Aceptar',
+          }).then(() => {
+            // Redirigir después de aceptar
+            this.router.navigate(['/dashboard/projects']);
+          });
+        },
+        (error) => {
+          Swal.fire({
+            title: 'Error',
+            text: 'Hubo un problema al guardar el proyecto.',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+          });
+        }
+      );
+    } else {
+      Swal.fire({
+        title: 'Campos vacíos',
+        text: 'No pueden haber campos vacíos.',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar'
       });
-    }else{
-      this.openDialog('No pueden haber campos vacios');
     }
   }
   openDialog(message: string): void {
